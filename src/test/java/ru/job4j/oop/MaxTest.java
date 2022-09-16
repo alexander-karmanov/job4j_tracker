e802606d9a9c835bbfc0fb95c9b45a7df5e537a8
alexander-karmanov <lektor_md@mail.ru>
2022-09-16 19:01:36 +0300

    Перемещение пакетов.  [#504871 #343906]

---
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1And2And3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax0And2And4And7Then7() {
+        int first = 0;
+        int second = 2;
+        int third = 4;
+        int fourth = 7;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+}
