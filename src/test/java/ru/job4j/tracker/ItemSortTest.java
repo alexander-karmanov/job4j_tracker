a2b6f1625dbf44f141d637bc1d7fd2fd0e6b89ce
alexander-karmanov <lektor_md@mail.ru>
2022-12-05 21:23:25 +0300

    0. Сортировка [#10096 #367413]

--- src/test/java/ru/job4j/tracker/ItemSortTest.java
+++ src/test/java/ru/job4j/tracker/ItemSortTest.java
@@ -4,37 +4,47 @@ import org.junit.Test;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ItemSortTest {
     @Test
     public void whenAscOrderSort() {
-        List<Item> items = new ArrayList<>();
-            items.add(new Item("Nick"));
-            items.add(new Item("Bob"));
-            items.add(new Item("John"));
+        List<Item> items = new ArrayList<>(
+                        Arrays.asList(
+                        new Item("Nick"),
+                        new Item("Bob"),
+                        new Item("John")
+                )
+        );
             items.sort(new ItemAscByName());
-        List<Item> expected = new ArrayList<>();
-            expected.add(new Item("Bob"));
-            expected.add(new Item("John"));
-            expected.add(new Item("Nick"));
-        assertThat(items.get(0).getName()).isEqualTo(expected.get(0).getName());
-        assertThat(items.get(1).getName()).isEqualTo(expected.get(1).getName());
-        assertThat(items.get(2).getName()).isEqualTo(expected.get(2).getName());
+
+        List<Item> expected = new ArrayList<>(
+                Arrays.asList(
+                        new Item("Bob"),
+                        new Item("John"),
+                        new Item("Nick")
+                )
+        );
+        assertArrayEquals(items.toArray(), expected.toArray());
         }
 
     @Test
     public void whenDescOrderSort() {
-        List<Item> items = new ArrayList<>();
-            items.add(new Item("Bob"));
-            items.add(new Item("Nick"));
-            items.add(new Item("John"));
+        List<Item> items = new ArrayList<>(
+                Arrays.asList(
+                        new Item("Bob"),
+                        new Item("Nick"),
+                        new Item("John")
+                )
+        );
             items.sort(new ItemDescByName());
-        List<Item> expected = new ArrayList<>();
-            expected.add(new Item("Nick"));
-            expected.add(new Item("John"));
-            expected.add(new Item("Bob"));
-        assertThat(items.get(0).getName()).isEqualTo(expected.get(0).getName());
-        assertThat(items.get(1).getName()).isEqualTo(expected.get(1).getName());
-        assertThat(items.get(2).getName()).isEqualTo(expected.get(2).getName());
+        List<Item> expected = new ArrayList<>(
+                Arrays.asList(
+                        new Item("Nick"),
+                        new Item("John"),
+                        new Item("Bob")
+                )
+        );
+        assertArrayEquals(items.toArray(), expected.toArray());
     }
 }
