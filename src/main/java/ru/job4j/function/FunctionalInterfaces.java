46b89896ee46824a9e19c189e97735a555556a29
alexander-karmanov <lektor_md@mail.ru>
2022-12-26 21:47:49 +0300

    2.2. Функциональный интерфейс [#247497]

---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,33 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (s1, s) -> map.put(s1, s);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
+        BiPredicate<Integer, String> biPred = (i, s) -> (i % 2 == 0) || map.get(i).length() == 4;
+        for (Integer i : map.keySet()) {
+            String st = biPred.test(i, map.get(i)) ? " value: " + map.get(i) : "";
+            System.out.println("key: " + i + " value: " + st);
+        }
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = s -> s.toUpperCase();
+        for (String s : strings) {
+            con.accept(func.apply(s));
+        }
+    }
+}
