201bea0bd5cdd598bcb6117e7dce381accbcb5dc
alexander-karmanov <lektor_md@mail.ru>
2023-01-07 18:28:37 +0300

    6. Список адресов [#110062 #377846]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile :: getAddress)
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
