398171953c241f5f172b0ae05f3416f8439102b7
alexander-karmanov <lektor_md@mail.ru>
2023-01-14 15:05:55 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -2,7 +2,8 @@ package ru.job4j.collection;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,28 +11,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> input = List.of("k1/sk1", "k2/sk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
 
     @Test
     public void whenMissedSomeDepartmentsCode() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -46,7 +47,7 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAscWithoutMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -57,7 +58,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
@@ -68,13 +69,17 @@ public class DepartmentsTest {
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2"
         );
-        Departments.sortAsc(input);
-        assertThat(input).containsSequence(expect);
+        List<String> input1 = new ArrayList<>();
+        Collections.copy(input, input1);
+        List<String> expect1 = new ArrayList<>();
+        Collections.copy(expect, expect1);
+        Departments.sortAsc(input1);
+        assertThat(input1).containsSequence(expect1);
     }
 
     @Test
     public void whenSortAscWithMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1",
@@ -82,7 +87,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K1/SK2",
@@ -90,13 +95,17 @@ public class DepartmentsTest {
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2"
         );
-        Departments.sortAsc(input);
-        assertThat(input).containsSequence(expect);
+        List<String> input1 = new ArrayList<>();
+        Collections.copy(input, input1);
+        List<String> expect1 = new ArrayList<>();
+        Collections.copy(expect, expect1);
+        Departments.sortAsc(input1);
+        assertThat(input1).containsSequence(expect1);
     }
 
     @Test
     public void whenSortDescWithoutMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -107,7 +116,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2",
                 "K2/SK1",
                 "K2/SK1/SSK1",
@@ -118,13 +127,17 @@ public class DepartmentsTest {
                 "K1/SK1/SSK2",
                 "K1/SK2"
         );
-        Departments.sortDesc(input);
-        assertThat(input).containsSequence(expect);
+        List<String> input1 = new ArrayList<>();
+        Collections.copy(input, input1);
+        List<String> expect1 = new ArrayList<>();
+        Collections.copy(expect, expect1);
+        Departments.sortDesc(input1);
+        assertThat(input1).containsSequence(expect1);
     }
 
     @Test
     public void whenSortDescWithMissedDepartments() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1",
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
@@ -133,7 +146,7 @@ public class DepartmentsTest {
                 "K2/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K2/SK1",
                 "K2/SK1/SSK1",
                 "K2/SK1/SSK2",
@@ -142,7 +155,11 @@ public class DepartmentsTest {
                 "K1/SK1/SSK2",
                 "K1/SK2"
         );
-        Departments.sortDesc(input);
-        assertThat(input).containsSequence(expect);
+        List<String> input1 = new ArrayList<>();
+        Collections.copy(input, input1);
+        List<String> expect1 = new ArrayList<>();
+        Collections.copy(expect, expect1);
+        Departments.sortDesc(input1);
+        assertThat(input1).containsSequence(expect1);
     }
 }
