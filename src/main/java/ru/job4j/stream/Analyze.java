567e9550309e1ef3cfbd6c14d2916f8722373a5c
alexander-karmanov <lektor_md@mail.ru>
2023-01-24 15:42:31 +0300

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -42,7 +42,7 @@ public class Analyze {
     public static Tuple bestSubject(Stream<Pupil> stream) {
         return stream
                 .flatMap(pupil -> pupil.subjects().stream())
-                .collect(Collectors.groupingBy(Subject::name, LinkedHashMap::new, Collectors.summingDouble(Subject::score)))
+                .collect(Collectors.groupingBy(Subject::name, Collectors.summingDouble(Subject::score)))
                 .entrySet().stream()
                 .map(pupil -> new Tuple(pupil.getKey(), pupil.getValue()))
                 .max(Comparator.comparing(Tuple::score))
